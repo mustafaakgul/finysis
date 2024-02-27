1fadaa9c775d814fc68e5980281ab363b3f02029
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-02-28 02:02:40 +0300

    Add currency tests

--- apps/currencies/tests.py
+++ apps/currencies/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
-# Create your tests here.
+
+class CurrenciesTests(SimpleTestCase):
+
+    def test_usd_currency(self):
+        self.assertEqual('a', 'a')
+
+    # def test_usd_currency(self):
+    #     currency_object = CurrencyExchange()
+    #     usd_value = currency_object.get_current_rate('USD', 'BanknoteBuying')
+    #
+    #     self.assertEqual('a', usd_value)
+
