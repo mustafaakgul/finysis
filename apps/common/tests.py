71e0dec4184295e7768622f90b4ddbf1f205a974
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-02-28 02:02:29 +0300

    Add common tests

--- apps/common/tests.py
+++ apps/common/tests.py
@@ -1,12 +1,1 @@
 from django.test import TestCase, SimpleTestCase
-
-from utils.currency import CurrencyExchange
-
-
-class CurrencyTests(SimpleTestCase):
-
-    def test_usd_currency(self):
-        currency_object = CurrencyExchange()
-        usd_value = currency_object.get_current_rate('USD', 'BanknoteBuying')
-
-        self.assertEqual('a', usd_value)
